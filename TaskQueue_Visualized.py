6ba6f0276abcc233785a783a1796b2694ef5b90e
Chad Rivera <112841372+chadriverae@users.noreply.github.com>
2023-09-21 00:30:48 -0400

    Add all files

---
+++ TaskQueue_Visualized.py
@@ -0,0 +1,155 @@
+import matplotlib.pyplot as plt
+import time
+class Task():
+    def __init__(self, id, cycles_left):
+        self.id = id
+        self.cycles_left = cycles_left
+        self.prev = self.next = None
+
+    def reduce_cycles(self, amount):
+        self.cycles_left -= amount
+        if self.cycles_left < 0:
+            self.cycles_left = 0
+    
+class TaskQueue():
+    def __init__(self, cycles_per_task=1):
+        self.current = None
+        self.cycles_per_task = cycles_per_task
+        self.length = 0
+        self.id_dict = {}
+        
+    def len(self):
+        return self.length
+    
+    def is_empty(self):
+        return self.length == 0
+    
+    def add_task(self, task):
+        # Case 1 (1st task to be added)
+        if self.is_empty():
+            self.current = self.current.prev = self.current.next = task
+        # Case 2 (2nd Task to be added)
+        elif self.length == 1:
+            task.prev = task.next = self.current
+            self.current.prev = self.current.next = task
+        # Case 3 (Squeeze!)
+        else:
+            task.next = self.current
+            task.prev = self.current.prev
+            self.current.prev.next = task
+            self.current.prev = task # Sandwich
+        
+        self.id_dict[task.id] = task
+        self.length += 1
+    
+    def remove_task(self, id):
+        """Removes the task with a given id"""
+        if id in self.id_dict:
+            task = self.id_dict[id]
+            if self.length == 1:
+                self.current = None
+            else: 
+                if self.current == task:
+                    self.current = self.current.next
+                task.prev.next = task.next
+                task.next.prev = task.prev
+            del task    # Wipe task from existance!
+            self.id_dict.pop(id)
+            self.length -= 1
+        else:
+            raise RuntimeError(f'id {id} not in TaskQueue')
+        
+    def execute_tasks(self):    
+        # def current_idx:
+        #     if clock_cycles == 0:
+        #         return 0
+        #     else:
+        #         return (clock_cycles % self.length) - amount_completed
+        def redraw():
+            plt.pie(sizes, labels = labels, startangle = 90,
+            colors = colors, counterclock = False)
+            plt.draw()
+            plt.pause(.2)
+            time.sleep(.2)
+        clock_cycles = 0
+        labels = []
+        sizes = []
+        colors = []
+        amount_completed = 0
+        for i in range(self.length):
+                labels.append(f'Task {self.current.id}: {self.current.cycles_left}')
+                self.current = self.current.next
+        for i in range(len(labels)):
+            sizes.append(100/len(labels))
+        for i in range(len(labels)):
+            colors.append('gray')
+        current_idx = 0
+        while self.length != 0:
+            print(labels)
+            print("Switch")
+            print('Clock Cycles',clock_cycles)
+            print('TQ length', self.length)
+            print("Current idx", current_idx)
+            if self.length > 1:
+                colors[current_idx - 1] = "gray"
+            colors[current_idx] = "red"
+            # print(colors)
+            redraw()
+            plt.close()
+
+            if self.cycles_per_task >= self.current.cycles_left:
+                temp = self.current.cycles_left
+                self.current.reduce_cycles(self.cycles_per_task)
+
+                labels[current_idx] = f'Task {self.current.id}: {self.current.cycles_left}'
+                redraw()
+                plt.close()
+                del labels[current_idx]
+                del sizes[current_idx]
+                del colors[current_idx]
+
+                clock_cycles += temp
+                print(f'Finished Task {self.current.id} after {clock_cycles} clock cycles')
+                self.remove_task(self.current.id)
+                amount_completed += 1
+                print('ac',amount_completed)
+               
+            else: 
+                self.current.reduce_cycles(self.cycles_per_task)
+
+                labels[current_idx] = f'Task {self.current.id}: {self.current.cycles_left}'
+                redraw()
+
+                clock_cycles += self.cycles_per_task
+                self.current = self.current.next
+                current_idx += 1 #Visual
+            if current_idx == self.length:
+                current_idx = 0
+            
+        return clock_cycles
+
+    def show_queue(self):
+        labels = []
+        for i in range(self.length):
+                labels.append(f'Task {self.current.id}: {self.current.cycles_left}')
+                self.current = self.current.next
+        sizes = []
+        for i in range(len(labels)):
+            sizes.append(100/len(labels))
+        plt.pie(sizes, labels = labels, startangle = 90, counterclock = False)
+        plt.show()
+
+# TQ3 = TaskQueue()
+# tasks = [Task(1, 3), Task(2, 1), Task(3, 5), Task(4,9)]
+# for task in tasks:  
+#     TQ3.add_task(task)
+# # TQ3.show_queue()
+# TQ3.execute_tasks()
+
+
+# TQ3 = TaskQueue(3)
+# tasks = [Task(1, 3), Task(2, 4), Task(3, 7), Task(4,9)]
+# for task in tasks:  
+#     TQ3.add_task(task)
+# # TQ3.show_queue()
+# TQ3.execute_tasks()
---
+++ TaskQueue_use.py
@@ -0,0 +1,42 @@
+from TaskQueue_Visualized import *
+print("Hello! Welcome to create-a-task-Queue!")
+cycles_per_task = int(input('How many cycles would you like to reduce per task:'))
+TQ = TaskQueue(cycles_per_task)
+while True:
+    print("Choose from the options below")
+    print("1. Execute Tasks")
+    print("2. Show Tasks")
+    print("3. Add Task")
+    print("4. Remove Task")
+    choice = int(input())
+    
+    if choice == 1:
+        TQ.execute_tasks()
+        break
+
+    if choice == 2:
+        TQ.show_queue()
+        print()
+    
+    if choice == 3:
+        while True:
+            print("Type \"q\" to exit to main screen")
+            task_id = input("Enter ID of task you would like to add:")
+            if task_id == "q":
+                break
+            cycles = int(input("Enter amount of cycles needed to complete task:"))
+            TQ.add_task(Task(task_id, cycles))
+            print("Task Added!\n")
+        
+    if choice == 4:
+        task_id = input("Enter ID of task you would like to remove")
+        TQ.remove_task(task_id)
+        print("Task Removed!\n")
+
+
+
+# TQ3 = TaskQueue()
+# tasks = [Task(1, 3), Task(2, 1), Task(3, 5)]
+# for task in tasks:  
+#     TQ3.add_task(task)
+# TQ3.execute_tasks() 
